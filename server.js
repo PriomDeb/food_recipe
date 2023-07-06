2f43ca6f6b1250a8d9274dbcab7704d4f7a13aa8
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-07-06 12:19:24 +0600

    Added authController and authRout

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
 
 
 dotenv.config();
@@ -17,6 +18,10 @@ connectDB();
 app.use(express.json())
 app.use(morgan('dev'))
 
+// Routes
+app.use('/api/v1/auth', authRoutes);
+
+// Rest API
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to Recipe Website.</h1>")
 });
