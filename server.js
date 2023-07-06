64d3967527f46d15d0e07695bb7dd9d3ceff5a32
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-07-06 11:16:02 +0600

    Added .env variables to secure port

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import colors from 'colors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
@@ -7,8 +10,8 @@ app.get("/", (req, res) => {
     res.send("<h1>Welcome to Recipe Website.</h1>")
 });
 
-const PORT = 9090
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`.bgCyan.white);
+    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 })
