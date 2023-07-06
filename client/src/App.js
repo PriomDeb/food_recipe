50c4b6d7fd76a610d4a6dcd4a8dc683ebd703591
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-07-06 21:15:37 +0600

    Implemented Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import { Layout } from './components/Layout/Layout';
+import {Routes, Route} from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { PageNotFound } from './pages/PageNotFound';
 
 function App() {
   return (
     <>
-    <Layout>
-    <h1>Food Recipe</h1>
-    </Layout>
+
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+    </Routes>
+    
     </>
   );
 }
