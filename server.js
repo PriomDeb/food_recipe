1882cb64de5de0a238e211b6371706fec5266b48
Shammo <58135476+Arc4NE01@users.noreply.github.com>
2023-07-06 13:56:17 +0600

    changed the color

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js'
-
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 
 // Rest object
-const app = express()
+const app = express();
 
 // Database Configuration
 connectDB();
 
 // Middleware
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
-app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 // Rest API
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to Recipe Website.</h1>")
+  res.send("<h1>Welcome to Recipe Website.</h1>");
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-})
+  console.log(
+    `Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.blue
+  );
+});
