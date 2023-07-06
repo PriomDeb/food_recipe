319cc716f8e982235e4d422fb81b6be5396a41c8
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-07-06 11:58:13 +0600

    Connection of MongoDB Database

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
 
 dotenv.config();
 
+// Rest object
 const app = express()
 
+// Database Configuration
+connectDB();
+
+// Middleware
+app.use(express.json())
+app.use(morgan('dev'))
+
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to Recipe Website .</h1>")
+    res.send("<h1>Welcome to Recipe Website.</h1>")
 });
 
 const PORT = process.env.PORT || 8080;
