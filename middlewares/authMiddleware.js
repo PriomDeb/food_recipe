76cb6ff19d01f8fe1966697fb44f04c8df114d3f
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-07-06 14:03:26 +0600

    Checkpoint 01
    Now react application implementation will be started.

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -29,7 +29,7 @@ export const isAdmin = async(req, res, next) => {
         res.status(501).send({
             success:false,
             error,
-            message:'Error in admin middleware'
+            message:'Error in admin middleware '
         })
     }
 }
