b7e0308ff8953b95873672f8d74077d292d0a31a
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-07-26 22:49:24 +0600

    Registration form completed

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,38 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../../components/Layout/Layout";
 
 const Register = () => {
+  const [name, SetName] = useState("");
+  const [email, SetEmail] = useState("");
+  const [phone, SetPhone] = useState("");
+  const [password, SetPassword] = useState("");
+  const [address, SetAddress] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, address, phone);
+  };
+
   return (
     <Layout>
       <div className="register">
         <h1>Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="exampleInputName" className="form-label">
               Name
             </label>
-            <input type="text" className="form-control" id="name" />
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={name}
+              onChange={(e) => SetName(e.target.value)}
+              required
+            />
           </div>
 
           <div className="mb-3">
             <label htmlFor="exampleInputName" className="form-label">
               Phone
             </label>
-            <input type="text" className="form-control" id="phone" />
+            <input
+              type="text"
+              className="form-control"
+              id="phone"
+              value={phone}
+              onChange={(e) => SetPhone(e.target.value)}
+              required
+            />
           </div>
 
           <div className="mb-3">
             <label htmlFor="exampleInputName" className="form-label">
               Address
             </label>
-            <input type="text" className="form-control" id="address" />
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              value={address}
+              onChange={(e) => SetAddress(e.target.value)}
+              required
+            />
           </div>
 
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
-            <input type="email" className="form-control" id="email" />
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              onChange={(e) => SetEmail(e.target.value)}
+              required
+            />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
             </div>
@@ -42,7 +81,14 @@ const Register = () => {
             <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
-            <input type="password" className="form-control" id="password" />
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => SetPassword(e.target.value)}
+              required
+            />
           </div>
 
           <button type="submit" className="btn btn-primary">
