0430fd70dfa2461aa502145c78fdcc956f7d1b26
Shammo <58135476+Arc4NE01@users.noreply.github.com>
2023-07-26 22:22:44 +0600

    created register page and added style

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import {Routes, Route} from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import { PageNotFound } from './pages/PageNotFound';
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { PageNotFound } from "./pages/PageNotFound";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <>
-
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='*' element={<PageNotFound/>}/>
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
