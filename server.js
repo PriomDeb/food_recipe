c376e03b6e1210bd72d2929276e2dea5033775d0
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2023-08-13 22:16:08 +0600

    Category API CRUD

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js'
-import cors from 'cors'
-
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+import categoryRoute from "./routes/categoryRoute.js";
 
 dotenv.config();
 
 // Rest object
-const app = express()
+const app = express();
 
 // Database Configuration
 connectDB();
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
-app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoute);
 
 // Rest API
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to Recipe Website.</h1>")
+  res.send("<h1>Welcome to Recipe Website.</h1>");
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-})
+  console.log(
+    `Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
